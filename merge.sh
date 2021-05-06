a0f6f3346129d20f9335005a88b2319b2bb31326
dororich <a113.b0b@gmail.com>
2021-05-06 16:51:51 +0900

    fix merge shellscript

--- merge.sh
+++ merge.sh
@@ -1,9 +1,9 @@
-for repo in intro-curriculum-30xx; do
+branch=master
+repo=intro-curriculum-30xx
 git remote add ${repo} ../${repo}
 git fetch ${repo}
-git read-tree --prefix=${repo} ${repo}/master
+git read-tree --prefix=${repo} ${repo}/${branch}
 git checkout -- .
 git add .
 git commit -m "add ${repo}"
-git merge -s subtree ${repo}/master --allow-unrelated-histories
-done
+git merge -s subtree ${repo}/${branch} --allow-unrelated-histories
