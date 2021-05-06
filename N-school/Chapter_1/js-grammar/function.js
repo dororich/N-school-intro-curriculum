5baef731cd640f01600763ab3553bd32e9b2468e
dororich <a113.b0b@gmail.com>
2021-05-06 17:30:34 +0900

    add N-school

---
+++ N-school/Chapter_1/js-grammar/function.js
@@ -0,0 +1,7 @@
+var myBirthTime = new Date(1992, 04, 09, 23, 45);
+function updateParagraph() {
+    var now = new Date();
+    var seconds = (now.getTime() - myBirthTime.getTime()) / 1000;
+    document.getElementById('birth-time').innerText = '生まれてから' + seconds + '秒経過。';
+}
+setInterval(updateParagraph, 50);
---
+++ N-school/Chapter_1/myFirst.js
@@ -0,0 +1,3 @@
+document.write('1足す2は');
+ocument.write(1 + 2);
+document.write('です')
